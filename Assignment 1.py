21a4a519b1972d21eba25c17aa6de3d46df3b5d3
Legg Yeung <chucooleg@gmail.com>
2021-01-25 23:22:36 -0500

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,3 +1,18 @@
+"""
+When sharing a link with a friend have you ever noticed the seemingly random letters and numbers that shortened links are usually made up of?
+
+https://youtu.be/LpuPe81bc2w
+
+In most cases, it's just a base-62 encoded integer. 
+
+In this assignment, you're being tasked to write a pair of functions to convert a base-62 number to base-10 and back again.
+
+Hint: Use an array/string that contains all the numbers and digits: "0123456789AB...YZab...yz". For each letter in the base-62 number, look up the value in the string ("0123456789ABC..."). This gives you the base-62 value for that digit. Keep a running total and remember to keep multiplying by 62
+"""
+
+
+###### My solution
+
 base_62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 # 1. For our first problem, write a function that converts a given base-62 string
@@ -23,17 +38,31 @@ def to_base_62(number):
     ans = str(lsb) + ans
     number = number // 62
   return ans
-  
-  
-  
-"""
-When sharing a link with a friend have you ever noticed the seemingly random letters and numbers that shortened links are usually made up of?
 
-https://youtu.be/LpuPe81bc2w
 
-In most cases, it's just a base-62 encoded integer. 
+###### Below is Teacher's solution
 
-In this assignment, you're being tasked to write a pair of functions to convert a base-62 number to base-10 and back again.
+base_62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-Hint: Use an array/string that contains all the numbers and digits: "0123456789AB...YZab...yz". For each letter in the base-62 number, look up the value in the string ("0123456789ABC..."). This gives you the base-62 value for that digit. Keep a running total and remember to keep multiplying by 62
-"""
+# 1. For our first problem, write a function that converts a given base-62 string
+#    into an integer. Only a single string will be provided, and it will be up to
+#    11 characters in length.
+
+def to_base_10(videoId):
+  base_10_total = 0
+  for letter in videoId:
+    val_62 = base_62.index(letter)
+    base_10_total = val_62 + (base_10_total * 62)
+  return base_10_total
+
+# 2. Write a function that does the opposite of the previous one. 
+# That is, it encodes a base 10 number into base 62
+# producing a string that represents the same number.
+
+def to_base_62(number):
+  output_string = ''
+  base = len(base_62)
+  while(number > 0):
+    output_string = base_62[number % base] + output_string 
+    number = number // base
+  return output_string
